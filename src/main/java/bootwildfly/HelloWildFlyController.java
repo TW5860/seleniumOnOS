17ea7cc7486dff7a969ff279208d2679274d013e
kofl91 <kimfehrs@gmail.com>
2018-02-23 13:50:35 +0100

    Add Selenium Endpoint

--- src/main/java/bootwildfly/HelloWildFlyController.java
+++ src/main/java/bootwildfly/HelloWildFlyController.java
@@ -1,5 +1,7 @@
 package bootwildfly;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +12,16 @@ public class HelloWildFlyController {
     @RequestMapping("hello")
     public String sayHello(){
         return ("Hello, SpringBoot on Wildfly");
+    }
+
+    @RequestMapping("uitest")
+    public String startSelenium(){
+        try {
+            WebDriver driver = new HtmlUnitDriver();
+            driver.get("http://google.de");
+            return driver.getTitle();
+        }catch (Exception e){
+            return "Fail!";
+        }
     }
 }
