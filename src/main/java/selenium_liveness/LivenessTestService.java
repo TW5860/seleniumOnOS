05b40cf7b93cbb7b9e994fa9ca187b84d4a98cbc
Simon Weiler <sweiler@thoughtworks.com>
2018-03-09 11:27:47 +0100

    Revert "try adding .close() too"
    This reverts commit 426181386bf09ea49628f778675a3a2915170e56.

--- src/main/java/selenium_liveness/LivenessTestService.java
+++ src/main/java/selenium_liveness/LivenessTestService.java
@@ -88,7 +88,6 @@ public class LivenessTestService {
             driver.findElement(By.cssSelector("#tsf > div.tsf-p > div.jsb > center > input[type=\"submit\"]:nth-child(2)")).click();
             LOG.info("Browser title is now: {}", driver.getTitle());
         } finally {
-            driver.close();
             driver.quit();
         }
     }
