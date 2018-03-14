2284e973ef52cd43968887f2729513bb86addce1
Simon Weiler <sweiler@thoughtworks.com>
2018-03-14 17:22:03 +0100

    add configuration for parallel thread execution

--- src/main/java/selenium_liveness/PrometheusConfiguration.java
+++ src/main/java/selenium_liveness/PrometheusConfiguration.java
@@ -1,11 +1,19 @@
 package selenium_liveness;
 
 import io.prometheus.client.spring.boot.EnablePrometheusEndpoint;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 
 @Configuration
 @EnablePrometheusEndpoint
 public class PrometheusConfiguration {
 
+    @Bean
+    public ScheduledExecutorService scheduledExecutorService() {
+        return Executors.newScheduledThreadPool(5);
+    }
 
 }
